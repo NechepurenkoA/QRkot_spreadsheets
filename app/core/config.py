9623cc11eda2aaf2fe06a6cb7c0e7f6df9cd717c
NechepurenkoA <nechepurenko.work@gmail.com>
2023-11-15 21:23:45 +0300

    added app title, description

--- app/core/config.py
+++ app/core/config.py
@@ -4,8 +4,8 @@ from pydantic import BaseSettings, EmailStr
 
 
 class Settings(BaseSettings):
-    app_title: str
-    description: str
+    app_title: str = 'Фонд помощи QRкот'
+    description: str = 'API для работы с приложением по помощи котикам.'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr] = None
